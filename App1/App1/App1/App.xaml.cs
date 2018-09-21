f45b5c16ea66f13619d668acbd207e242e225c78
rafael meran <rtulio007@gmail.es>
2018-09-20 22:30:08 -0400

    Completo

---
+++ App1/App1/App1/App.xaml.cs
@@ -0,0 +1,33 @@
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using App1.Views;
+
+[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
+namespace App1
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new NavigationPage( new LoginPage());
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ App1/App1/App1/InfraStructor/InstanceLocator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using App1.ViewModels;
+
+namespace App1.InfraStructor
+{
+    public class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+
+
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+
+    
+}
