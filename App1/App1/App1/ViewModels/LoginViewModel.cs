924f2ebf20db6b99b4e602dfc2c083b17672b45e
rafael meran <tulio007@hotmail.es>
2018-09-23 13:02:53 -0400

    Con parte del LoginViewModel

--- App1/App1/App1/ViewModels/LoginViewModel.cs
+++ App1/App1/App1/ViewModels/LoginViewModel.cs
@@ -1,23 +1,81 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace App1.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel: BaseViewModel
     {
-        #region Propiedades
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public bool IsRemember { get; set; }
-        public bool IsRunning { get; set; }
+        #region Atributos
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+
+
         #endregion
 
+        #region Propiedades
+        public string Email { get; set; }
+        public string Password
+        {
+            get { return password; }
+
+            set { SetValue(ref password, value); }
+        }
+
+        public bool IsRemember { get; set; }
+        public bool IsRunning
+        {
+            get { return isRunning; }
+
+            set { SetValue(ref isRunning, value); }
+
+        }
+        public bool IsEnabled
+        {
+            get { return isEnabled; }
+
+            set { SetValue(ref isEnabled, value); }
+        }
+        #endregion
 
         #region Comandos
 
-        public ICommand SignIn { get; set; }
+        public ICommand SignInCommand
+        {
+            get
+            {
+                return new RelayCommand(SignIn);
+
+            }
+        }
+
+        private async void SignIn()
+        {
+
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Email is empty", "Ok");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Password is empty ", "Ok");
+                return;
+            }
+
+            if(this.Email != "tulio007@hotmail.es" || this.Password != "guapo12")
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Password or Email is incorrect.", "Accept");
+                this.Password = string.Empty;
+                return;
+            }
+
+        }
 
 
         #endregion
@@ -27,6 +85,9 @@ namespace App1.ViewModels
         public LoginViewModel()
         {
             this.IsRemember = true;
+            this.Email = "tulio007@hotmail.es";
+            this.Password = "guapo12";
+            this.IsEnabled = true;
         }
 
         #endregion
